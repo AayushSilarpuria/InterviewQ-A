114a92453569d8736c9c54cbe8731bc00dbd96a8
Ayush <ayushsilarpuria007@gmail.com>
2023-04-20 00:02:43 +0530

    Modified SRM Technology

--- AyushSilarpuria/SRM_Technology.py
+++ AyushSilarpuria/SRM_Technology.py
@@ -12,3 +12,13 @@ class Students:
 
 d = Students("Ayush", 21)
 d.stud()
+
+
+
+# Write code to open Excel file then read it:
+
+with open("file1.xlsx", mode='r') as pd:
+    print(pd.read())
+
+with open("file.xlsx", mode='w') as pd:
+    pd.write("Hello World!")
