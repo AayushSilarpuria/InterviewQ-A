a8d52ba02eb01edae8684536a34e5ebf84d6e6ce
Ayush <ayushsilarpuria007@gmail.com>
2023-04-21 23:42:01 +0530

    New Company Promantia

---
+++ AyushSilarpuria/Promantia.py
@@ -0,0 +1,11 @@
+# Q-1: How you deploy project?
+# Q-2: Git- (1) Pull vs Fetch (2) How you merge history of base branch to new branch.
+# Q-3: Innumeraus model use.
+# Q-4: 
+# Q-5: Shuffle the number in the list.
+
+list1 = [2,4,5,6,9]
+
+from random import shuffle
+shuffle(list1)
+print(list1)
