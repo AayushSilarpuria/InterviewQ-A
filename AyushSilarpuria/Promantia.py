c42a2d6984ebb8a339af5366713eadd1fd427da8
Ayush <ayushsilarpuria007@gmail.com>
2023-04-22 09:00:41 +0530

    adding Promantia

--- AyushSilarpuria/Promantia.py
+++ AyushSilarpuria/Promantia.py
@@ -1,9 +1,10 @@
 # Q-1: How you deploy project?
 # Q-2: Git- (1) Pull vs Fetch (2) How you merge history of base branch to new branch.
 # Q-3: Innumeraus model use.
-# Q-4: 
-# Q-5: Shuffle the number in the list.
-
+# Q-4: Explain Oops concept.
+# Q-5: Explain abstraction with real life example.
+# Q-6: What is sort() and sorted() function?
+# Q-7: Shuffle the number in the list.
 list1 = [2,4,5,6,9]
 
 from random import shuffle
