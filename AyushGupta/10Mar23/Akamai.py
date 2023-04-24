7a23d35af700a224dd7a19494408e67d3bea0ee0
Ayush Gupta <gupta07ayush@gmail.com>
2023-04-25 00:41:37 +0530

    Akamai Sunovaa 10Mar

---
+++ AyushGupta/10Mar23/Akamai.py
@@ -0,0 +1,24 @@
+# Akamai
+# sunovaa tech
+# 1. Generator vs iterator
+
+# 2. how to find if a function is generator or not
+
+
+# 3. SQL: Employee(name, salary). find min salary
+
+# 4. Class method vs static method vs instance method
+
+
+# 5. When you use GET method for getting API;  explain the flow when the request get to the server.
+
+# 6.diff between tuple and list
+
+# 7. Which can be used as a key in dictionary- tuple or list.
+
+# 8. Find how many seconds it will take to run this function through generator
+def func():
+    print("Hello")
+func()
+
+
