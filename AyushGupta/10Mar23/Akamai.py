3f6cf6dfcb6da1fd2f94eb86a7e1be664f19438a
Ayush Gupta <gupta07ayush@gmail.com>
2023-04-25 00:44:27 +0530

    already provided questions

--- AyushGupta/10Mar23/Akamai.py
+++ AyushGupta/10Mar23/Akamai.py
@@ -22,3 +22,41 @@ def func():
 func()
 
 
+# Already provided questions
+# 1.       What is a list and tuples?
+
+# 2.       How do you dictionary Tuples and List?
+
+# 3.       How to migrate a list?
+
+# 4.       How to migrate data in Django?
+
+# 5.       Project explanation with Django?
+
+# 6.       Explain Viewed files?
+
+# 7.       What is the middle layer? Explain.
+
+# 8.       Coding – find the strong number
+
+# 9.       Projects and Roles and responsibilities of Current projects?
+
+# 10.   What are Methods?
+
+# 11.   How will you run a project in Django?
+
+# 12.   What are routines?
+
+# 13.   How to create views ?
+
+# 14.   What is the communication between Model file and Viewed file?
+
+
+# Some more questions
+# Back end API when you call the browser what happens ?
+# How do you update the column you want to add in the database?
+# What do you mean by list ?
+# What do you mean by pearl staes ?
+# What is a static method?
+# What are the project you have done with python and what is your responsibility in the project ?
+
