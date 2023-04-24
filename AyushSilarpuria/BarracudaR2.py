79173c46e6fd67148a5f71181e3f0b0803f20fc8
Ayush <ayushsilarpuria007@gmail.com>
2023-04-24 23:24:53 +0530

    Barracuda Round2 Interview

---
+++ AyushSilarpuria/BarracudaR2.py
@@ -0,0 +1,36 @@
+# Q.1: 
+
+list = [3,5,6,8,0]
+def new_list(list):
+    print(list)
+    
+    
+list_new = id(list)
+print(list_new)
+print(id(new_list))
+
+x=10
+print(id(x))
+x= 11
+print(id(x))
+
+
+
+def user_name(lists):
+    for i in range(len(lists)):
+        
+
+
+try:
+except:
+
+error:
+        
+
+dic = {1:'ayu', 2: 'raj'}
+try:
+    print(dic[1])
+except KeyError as e :
+    print(e)
+
+print("hello")
