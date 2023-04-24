b47ae6f53a74824a47ee5f2a2369fed69f018929
Ayush <ayushsilarpuria007@gmail.com>
2023-04-24 23:59:56 +0530

    Barracuda update

--- AyushSilarpuria/BarracudaR2.py
+++ AyushSilarpuria/BarracudaR2.py
@@ -1,13 +1,17 @@
-# Q.1: 
+# Q.1: Difference between mutable and immutable.
+# Q.2: Explain why or how string are immutable and list are mutable.
 
 list = [3,5,6,8,0]
 def new_list(list):
     print(list)
     
-    
+print() 
+
 list_new = id(list)
 print(list_new)
 print(id(new_list))
+
+print()
 
 x=10
 print(id(x))
@@ -15,12 +19,12 @@ x= 11
 print(id(x))
 
 
-
+# Q.3: Write a program to sort list and a dictionary in sequantial manner.
 def user_name(lists):
     for i in range(len(lists)):
         
 
-
+# Q.4: Explain excep
 try:
 except:
 
