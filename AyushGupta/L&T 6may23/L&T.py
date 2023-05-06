89fc85777c152368a286749930348863b705f5c0
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-06 12:32:59 +0530

    L&T 6may23

---
+++ AyushGupta/L&T 6may23/L&T.py
@@ -0,0 +1,29 @@
+# L&T 6may23 10:30 AM
+
+# Q1. What is field class in python.
+# Q2. test.client class
+# Q3. we have a csv file containing 1 million lines.
+# 	how will you divide the file in 100 100 lines.
+
+# Q4.
+price = {"Apple": 34, "Banana": 4, "Papaya": 45, "Peach": 65,
+         "Pomegranate": 50, "Pineapple": 45, "Strawberries": 5}
+my_purchase = {"Apple": 2, "Banana": 4,
+               "Papaya": 2, "Peach": 6, "Strawberries": 10}
+# write a program to get below output.
+Result = {"Apple": 68, "Banana": 16, "Papaya": 90,
+          "Peach": 390, "Strawberries": 100}
+
+# Q5. Plot a graph in python.
+# x =[1, 2, 3, 4, 5]
+# y =[2, 4, 6, 8, 10]
+
+# Q6.
+# what is the output of the program:
+
+
+def multiplexers():
+    return [lambda n: index * n for index in range(4)]
+
+
+print([m(2) for m in multiplexers()])
---
+++ AyushGupta/L&T 6may23/lamda.py
@@ -0,0 +1,7 @@
+def multiplexers():
+    return [lambda n: index * n for index in range(4)]
+
+
+print([m(2) for m in multiplexers()])
+
+# [6, 6, 6, 6]
