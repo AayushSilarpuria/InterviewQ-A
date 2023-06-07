b238a37da03c03c79418394c44e63a8a2ea4ef0a
Ayush <ayushsilarpuria007@gmail.com>
2023-06-07 14:15:20 +0530

    Update Program

--- AyushGupta/Akamai 10Mar23/Akamai.py
+++ AyushGupta/Akamai 10Mar23/Akamai.py
@@ -3,24 +3,65 @@
 # 1. Generator vs iterator
 
 # 2. how to find if a function is generator or not
+import inspect
+def foo():
+    return "foo"
+def bar():
+    yield "bar"
 
+print(inspect.isgeneratorfunction(foo))
+print(inspect.isgeneratorfunction(bar))
 
+# isclass, ismodule, isfunction, inspect.getmro(c) {To see class hierarchy.}
 # 3. SQL: Employee(name, salary). find min salary
 
 # 4. Class method vs static method vs instance method
 
+    # Instance methods need a class instance and can access the instance through self . 
+    # Class methods don't need a class instance. 
+    # They can't access the instance ( self ) but they have access to the class itself via cls . 
+    # Static methods don't have access to cls or self .
 
 # 5. When you use GET method for getting API;  explain the flow when the request get to the server.
+
+import requests
+
+response = requests.get("https://www.boredapi.com/api/activity")
+print(response.status_code)
+# 200 means Success.
+print(response.json())
+print()
+
+import json
+
+def jprint(obj):
+    text = json.dumps(obj, sort_keys =True, indent= 4)
+    print(text)
+
+jprint(response.json())
+print()
+
+action = response.json()['activity']
+jprint(action)
+
+print()
 
 # 6.diff between tuple and list
 
 # 7. Which can be used as a key in dictionary- tuple or list.
 
 # 8. Find how many seconds it will take to run this function through generator
+
+import timeit
+
+start = timeit.default_timer()
+
 def func():
     print("Hello")
 func()
 
+stop = timeit.default_timer()
+print("Time:", stop - start)
 
 # Already provided questions
 # 1.       What is a list and tuples?
